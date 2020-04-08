9afc9ad912d076e8f7464f02f631b05c6f376e50
lance-Pacleb <oland.pclb03190@yahoo.com>
2020-04-08 08:12:46 +0800

    New Update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
- 
+
+function initMap(){
+    var philippines = {
+        lat: 12.8797,
+        lng: 121.7740
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: philippines,
+        zoom: 7,
+        mapTypeId: 'roadmap',
+    });
+
+    var marker = new google.maps.Marker({
+        position: philippines,
+        map:map
+    });
+}
